2c2cc36b391f3b796c809e7241196f97ddf7266e
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-11-07 16:13:46 +0100

    bxslider not really finish but work

--- functions.php
+++ functions.php
@@ -6,13 +6,21 @@ wp_enqueue_style( 'style', get_stylesheet_uri());
 
 wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.css' );
 
+wp_enqueue_style('bxslider', get_stylesheet_directory_uri() . '/node_modules/bxslider/dist/jquery.bxslider.css');
+
 wp_enqueue_style( 'fontawesome', get_stylesheet_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/css/fontawesome.css' );
 
 wp_enqueue_script( 'jquery-dimitry', get_template_directory_uri() . '/node_modules/jquery/dist/jquery.js', array(), '20151215', true );
 
+wp_enqueue_script( 'jquery-dimitry', get_template_directory_uri(). '/node_modules/bxslider/dist/jquery-3.1.1.min.js');
+
 wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.js', array(), '20151215', true );
 
+wp_enqueue_script( 'bxslider', get_template_directory_uri() . '/node_modules/bxslider/dist/jquery.bxslider.js');
+
 wp_enqueue_script( 'fontawesome', get_template_directory_uri() . '/node_modules/@fortawesome/fontawesome-free/js/fontawesome.js', array(), '20151215', true );
+
+wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js');
 }
 
 add_action( 'wp_enqueue_scripts', 'CodiNShop_Style' );
--- header.php
+++ header.php
@@ -17,3 +17,25 @@
                 </section>
             </nav>
         </header>
+        <main>
+            <div class="slider">
+               <?php
+               $entete = new WP_Query( array('post_type' => 'product', 'product_cat' => 'slider' ));
+                   if ( $entete->have_posts() ) :
+
+                   while ( $entete->have_posts() ) : $entete->the_post();?>
+
+                   <div class="inslider">
+                       <?php the_post_thumbnail()?>
+
+                       <div class="slideinfo">
+                           <h2><?php the_title()?></h2>
+                           <p><?php the_content()?></p>
+                       </div>
+                   </div>
+
+               <?php
+                   endwhile;
+               endif;
+               ?>
+           </div>
